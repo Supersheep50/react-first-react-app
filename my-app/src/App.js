4fc72ef27c5a75cc428f00314bdd2f362d36cf4d
Supersheep50 <jonathan.morrisseyto@gmail.com>
2023-06-16 10:48:02 +0000

    Condtional rndering lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreeting';
-import StatefulGreetingWithPrevState from './components/StatefulGreeting';
-
-
-
-
-
+import "./App.css";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 
 function App() {
-  return (
-    <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="Nice to meet you!"/>
-      <StatefulGreeting greeting ="I'm a stateful class component!"/>
-      <StatefulGreetingWithCallback />
-      <StatefulGreetingWithPrevState />
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div className="App">
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional />
+  </div>);
 }
 
 export default App;
---
+++ my-app/src/components/ConditionalRenderingFunctional.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ConditionalRenderingFunctional() {
+  return (
+    <div>ConditionalRenderingFunctional</div>
+  )
+}
+
+export default ConditionalRenderingFunctional
